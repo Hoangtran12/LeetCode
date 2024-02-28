2e7a13eee0ce7f1b70de3c9c62e742cdf32ff50e
TisEm <49451623+Hoangtran12@users.noreply.github.com>
2024-02-28 14:04:12 -0600

    Time: 67 ms (26.98%) | Memory: 11.9 MB (51.64%) - LeetSync

---
+++ 383-ransom-note/ransom-note.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        st1, st2 = Counter(ransomNote), Counter(magazine)
+        if st1 & st2 == st1:
+            return True
+        return False        
