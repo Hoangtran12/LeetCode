de3f001ca41454126373b92f767f474ee7910b69
Hoang Tran <49451623+Hoangtran12@users.noreply.github.com>
2024-10-01 15:10:29 -0500

    Time: 30 ms (62.60%) | Memory: 12.5 MB (18.56%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        for i in range(len(nums)):
+            if target <= nums[i]:
+                return i
+
+        return len(nums)
+        
