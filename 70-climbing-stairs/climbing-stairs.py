1e3abc5318322fcd885929828e14e3a040e0f80f
Hoang Tran <49451623+Hoangtran12@users.noreply.github.com>
2024-10-10 15:00:35 -0500

    Time: 5 ms (96.07%) | Memory: 11.5 MB (64.94%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -2,11 +2,12 @@ class Solution(object):
     def climbStairs(self, n):
         if n <= 2:
             return n  # Handle base cases
+        x = 1
+        y = 2
+        z = 0
 
-        cs = [0] * (n + 1)
-        cs[0], cs[1] = 1, 1  # Base cases
-
-        for i in range(2, n + 1):
-            cs[i] = cs[i - 1] + cs[i - 2]  # Sum of the two previous steps
-
-        return cs[n]  # Return the number of ways to reach the nth step
+        for i in range(2, n):
+            z = x + y
+            x = y
+            y = z
+        return z
