f55544aab30ad051c2d8c1bc41417afd19188f0a
Hoang Tran <49451623+Hoangtran12@users.noreply.github.com>
2024-10-11 22:32:36 -0500

    Time: 45 ms (6.24%) | Memory: 16.5 MB (74.70%) - LeetSync

---
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def inorderTraversal(self, root):
+
+        return (
+            self.inorderTraversal(root.left)
+            + [root.val]
+            + self.inorderTraversal(root.right)
+            if root
+            else []
+        )
