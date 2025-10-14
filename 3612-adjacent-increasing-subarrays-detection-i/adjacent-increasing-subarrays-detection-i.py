d02a65930c0800d09488e3a68f94016af731a19f
Hoang Tran <49451623+Hoangtran12@users.noreply.github.com>
2025-10-14 09:08:22 -0500

    Time: 96 ms (16.04%) | Memory: 17.9 MB (25.67%) - LeetSync

--- 3612-adjacent-increasing-subarrays-detection-i/adjacent-increasing-subarrays-detection-i.py
+++ 3612-adjacent-increasing-subarrays-detection-i/adjacent-increasing-subarrays-detection-i.py
@@ -1,32 +1,15 @@
-from typing import List
-
 class Solution:
     def hasIncreasingSubarrays(self, nums: List[int], k: int) -> bool:
-        if len(nums) == 0:
-            return False
+        n = len(nums)
+        count, precount, ans = 1, 0, 0
 
-        # Step 1: Find the lengths of all consecutive increasing subarrays
-        lengths = []
-        count = 1
-        for i in range(1, len(nums)):
-            if nums[i] > nums[i - 1]:
-                count += 1
+        for i in range (1, n):
+            if nums[i] > nums[i-1]:
+                count +=1
             else:
-                lengths.append(count)
-                count = 1
-        lengths.append(count) # Add the last subarray length
-
-        # Step 2: Check the conditions on the list of lengths
-        # Check rule 1: Splitting a single subarray in half
-        for l in lengths:
-            if l // 2 >= k:
-                return True
-
-        # Check rule 2: Combining two adjacent subarrays
-        for i in range(1, len(lengths)):
-            l1 = lengths[i-1]
-            l2 = lengths[i]
-            if min(l1, l2) >= k:
-                return True
+                precount, count = count, 1
+            ans = max(ans, min(count, precount))
+            ans = max(ans, count //2)
         
-        return False
+        return ans >= k
+
