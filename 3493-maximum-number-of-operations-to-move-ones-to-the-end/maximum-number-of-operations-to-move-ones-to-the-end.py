07b4017ae6bfd838401266ff9ff5a46f8f380d81
Hoang Tran <49451623+Hoangtran12@users.noreply.github.com>
2025-11-13 10:34:10 -0600

    Time: 69 ms (44.91%) | Memory: 18.1 MB (81.44%) - LeetSync

--- 3493-maximum-number-of-operations-to-move-ones-to-the-end/maximum-number-of-operations-to-move-ones-to-the-end.py
+++ 3493-maximum-number-of-operations-to-move-ones-to-the-end/maximum-number-of-operations-to-move-ones-to-the-end.py
@@ -1,13 +1,12 @@
 class Solution:
     def maxOperations(self, s: str) -> int:
         n= len(s)
-        zero_segments = 0
+        ones_count = 0
         result = 0
 
-        for i in reversed(range(n)):
-            if s[i] == "0":
-                if (i == n - 1 or s[i+1] == "1"):
-                    zero_segments +=1
-            else:
-                result += zero_segments
+        for i in range(n):
+            if s[i] == "1":
+                ones_count +=1
+            elif s[i] == "0" and s[i-1] == "1":
+                result += ones_count
         return result
