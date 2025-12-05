fefdebfe688dff444d6f7c6d0fe75c136cf979d3
Hoang Tran <49451623+Hoangtran12@users.noreply.github.com>
2025-12-05 08:17:36 -0600

    Time: 0 ms (100.00%) | Memory: 17.5 MB (100.00%) - LeetSync

--- 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.py
+++ 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.py
@@ -1,11 +1,4 @@
 class Solution:
     def countPartitions(self, nums: List[int]) -> int:
-        left, count = 0, 0
-        right = sum(nums)
-
-        for num in nums[:-1]:
-            left += num
-            right -= num
-            if abs(left - right) % 2 == 0:
-                count +=1
-        return count
+        return len(nums)-1 if (sum(nums)&1)==0 else 0
+        
