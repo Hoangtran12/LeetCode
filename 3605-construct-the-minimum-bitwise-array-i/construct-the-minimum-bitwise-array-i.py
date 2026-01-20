ba09d4a12105c647077d30baa15210e89a47fd40
Hoang Tran <49451623+Hoangtran12@users.noreply.github.com>
2026-01-20 09:10:53 -0600

    Time: 0 ms (100.00%) | Memory: 19.3 MB (15.44%) - LeetSync

---
+++ 3605-construct-the-minimum-bitwise-array-i/construct-the-minimum-bitwise-array-i.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minBitwiseArray(self, nums: List[int]) -> List[int]:
+        for i, n in enumerate(nums):
+            if n % 2 == 0:
+                nums[i] = -1
+            else:
+                nums[i] = n - (( (n + 1) & -(n + 1) ) >> 1)
+                
+        return nums
