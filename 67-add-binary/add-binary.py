82c8d02b31296ff640b581fac5369e9eed728fc4
Hoang Tran <49451623+Hoangtran12@users.noreply.github.com>
2026-02-14 20:50:06 -0600

    Time: 0 ms (100.00%) | Memory: 19.5 MB (36.10%) - LeetSync

--- 67-add-binary/add-binary.py
+++ 67-add-binary/add-binary.py
@@ -1,9 +1,20 @@
-class Solution(object):
-    def addBinary(self, a, b):
-        """
-        :type a: str
-        :type b: str
-        :rtype: str
-        """
-        return bin(int(a, 2) + int(b, 2))[2:]
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        res = []
+        carry = 0
         
+        i, j = len(a) - 1, len(b) - 1
+        
+        while i >= 0 or j >= 0 or carry:
+            val_a = int(a[i]) if i >= 0 else 0
+            val_b = int(b[j]) if j >= 0 else 0
+            
+            total = val_a + val_b + carry
+            res.append(str(total % 2))
+            carry = total // 2
+            
+            i -= 1
+            j -= 1
+            
+        # Reverse the list and join to get the final string
+        return "".join(reversed(res))
