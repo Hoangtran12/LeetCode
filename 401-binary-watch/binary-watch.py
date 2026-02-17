e890570897bf8d8bef010bfdea813c894606eb80
Hoang Tran <49451623+Hoangtran12@users.noreply.github.com>
2026-02-17 12:19:13 -0600

    Time: 0 ms (100.00%) | Memory: 19.4 MB (39.62%) - LeetSync

---
+++ 401-binary-watch/binary-watch.py
@@ -0,0 +1,4 @@
+class Solution:
+    def readBinaryWatch(self, turnedOn: int) -> List[str]:
+        return [f"{h}:{m:02d}" for h in range(12) for m in range(60) 
+                if (h.bit_count() + m.bit_count()) == turnedOn]
